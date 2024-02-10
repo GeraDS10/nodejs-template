016300449642a23707e563c6c3d43efab2d09429
GeraDS10 <gerardosaldain10@gmail.com>
2024-02-09 23:29:47 -0300

    index.html added

--- server.mjs
+++ server.mjs
@@ -1,18 +1,15 @@
 import express from 'express';
 import { createServer } from 'http';
-import { resolve } from 'path';
+import { resolve, fileURLToPath } from 'path';
 
 const app = express();
 
-// Define el directorio estático
-const staticDir = resolve(__dirname, '../src');
-
-// Sirve los archivos estáticos desde el directorio 'src'
+// Obtener la ruta del directorio estático
+const staticDir = resolve(fileURLToPath(import.meta.url), '../src');
 app.use(express.static(staticDir));
 
 // Ruta de inicio
 app.get('/', (req, res) => {
-  // Envía el archivo HTML en la ruta de inicio
   res.sendFile(resolve(staticDir, 'index.html'));
 });
 
