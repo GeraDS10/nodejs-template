71db0c513961deda74d8be9adc6fc1dfa7602f43
GeraDS10 <gerardosaldain10@gmail.com>
2024-02-09 23:41:46 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,18 +5,18 @@ const { getPerros, createPerros, getPerroById, getPerroByNombre, deletePerro, ge
 
 router.get('/perros', getPerros);
 router.get('/perros/:id', getPerroById);
-router.get('/perro/:nombre', getPerroByNombre)
+router.get('/perro/:nombre', getPerroByNombre);
 router.post('/perros', createPerros);
-router.delete('/perros/:id', deletePerro)
+router.delete('/perros/:id', deletePerro);
 
 router.get('/estadias', getEstadias);
 router.get('/estadias/:id', getEstadiaById);
 router.post('/estadias', createEstadias);
-router.delete('/estadias/:id', deleteEstadia)
+router.delete('/estadias/:id', deleteEstadia);
 
 router.get('/peluquerias', getPeluquerias);
 router.get('/peluquerias/:id', getPeluqueriaById);
 router.post('/peluquerias', createPeluquerias);
-router.delete('/peluquerias/:id', deletePeluqueria)
+router.delete('/peluquerias/:id', deletePeluqueria);
 
 module.exports = router;
